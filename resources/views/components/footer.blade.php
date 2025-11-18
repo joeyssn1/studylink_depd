9026435db62e8473f1330455ade74eeb43e8ffc8
Clem-prog <clementisjs747@gmail.com>
2025-11-18 18:01:53 +0700

    pisah header footer jdi components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<!-- FOOTER -->
+    <footer class="w-full bg-[#22335F] text-white py-4 text-center text-sm">
+        © 2025 Team@DEPD — Universitas Ciputra Surabaya
+    </footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,22 @@
+<!-- HEADER -->
+    <header class="w-full bg-[#22335F] text-white">
+        <div class="max-w-5xl mx-auto flex items-center justify-between py-4 px-4">
+
+            <!-- Logo sementara -->
+            <div class="flex items-center space-x-2">
+                <div class="w-7 h-7 bg-white rounded-md flex items-center justify-center">
+                    <span class="text-[#22335F] font-bold text-sm">SL</span>
+                </div>
+                <h1 class="text-xl font-semibold">StudyLink</h1>
+            </div>
+
+            <!-- Navigation button -->
+            <nav class="flex items-center space-x-6 text-white">
+                <a href="#" class="border-b-2 border-white pb-1">Home</a>
+                <a href="#" class="hover:underline">Study</a>
+                <a href="#" class="hover:underline">History</a>
+                <a href="#" class="hover:underline">Profile</a>
+            </nav>
+
+        </div>
+    </header>
